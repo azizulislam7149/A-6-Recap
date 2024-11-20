674938035b97f757a274e1a0fb8ac9af6eb5170c
azizulislam7149 <md.azizulislam7149@gmail.com>
2024-11-20 18:17:40 +0600

    add all Assignment code

--- js/main.js
+++ js/main.js
@@ -12,6 +12,7 @@ const data = await res.json();
 
 setTimeout(()=>{
     displayPets(data.pets);
+    storedPetssData=data.pets;
     loadingSpinner(false);
 },2000)
 }
@@ -24,9 +25,45 @@ const loadPetsByCategory = async (element)=>{
     
     setTimeout(()=>{
         displayPets(data.data);
+        storedPetssData=data.data;
         loadingSpinner(false);
     },2000)
 }
+
+const loadPetDetails = async (id)=>{
+    
+    const res = await fetch(`https://openapi.programming-hero.com/api/peddy/pet/${id}`);
+    const data = await res.json()
+    
+    displayPetDetails(data.petData);
+}
+const displayPetDetails = (data)=>{
+    const modalBody = document.getElementById('details-container');
+    modalBody.innerHTML= ` 
+    <img class="h-60 object-cover w-full rounded-lg" src="${data.image}" alt="">
+    <h3>${data.pet_name}</h3>
+    <div class="flex items-start gap-4">
+    <div>
+    <p><i class="fa-solid fa-paw"></i>Breed: ${data.breed ? data.breed :'Not Available'}</p>
+    <p><i class="fa-solid fa-transgender"></i>Gender: ${data.gender ? data.gender :'Not Available'}</p>
+    <p><i class="fa-solid fa-syringe"></i>vaccinated_status: ${data.vaccinated_status ? data.vaccinated_status :'Not Available'}</p>
+    
+    </div>
+    <div>
+    <p><i class="fa-solid fa-calendar-days"></i>date_of_birth: ${data.date_of_birth ? data.date_of_birth :'Not Available'}</p>
+    <p><i class="fa-solid fa-dollar-sign"></i>price: ${data.price ? data.price :'Not Available'}</p>
+    </div>
+    
+    </div>
+    <Hr class="my-3"/>
+    <h3>Details information</h3>
+    <p>${data.pet_details ?data.pet_details : 'Not Available'}</p>
+
+    `
+    my_modal_2.showModal();
+
+}
+
 
 const displayCategories = (information)=>{
 
@@ -45,6 +82,21 @@ information.forEach(element => {
 }
 const displayPets = (information)=>{
 const petContainers = document.getElementById('all-pets');
+if(information.length=== 0){
+    petContainers.classList.remove('grid');
+    petContainers.innerHTML= `
+    <div class="bg-gray-400 p-20 rounded-xl text-center space-y-4 ">
+    <i class="fa-brands fa-instagram mx-auto"></i>
+    <h3 class="text-3xl">No data available</h3>
+    <p>there is no informmation in this button</p>
+</div>
+    `
+    return
+}
+else{
+    petContainers.classList.add('grid');
+}
+   
 petContainers.innerHTML= '';
 information.forEach(elements =>{
 const div = document.createElement('div');
@@ -58,14 +110,34 @@ div.innerHTML= `
 <p class="text-white text-sm">Price: <span>$</span> ${elements.price ? elements.price :'Not Available'}</p>
 </hr class="my-2">
 <div class="flex justify-between items-center px-2 gap-3">
-    <button class="btn bg-white text-teal-700 border rounded-lg py-1 px-4"><i class="fa-solid fa-thumbs-up"></i></button>
-    <button class="btn bg-white text-teal-700 border rounded-lg py-1 px-4">Adopt</button>
-    <button class="btn bg-white text-teal-700 border rounded-lg py-1 px-4">Details</button>
+    <button onclick="like('${elements.image}')" class="btn bg-white text-teal-700 border rounded-lg py-1 px-4"><i class="fa-solid fa-thumbs-up"></i></button>
+    <button onclick="adoptModal(this)" class="btn bg-white text-teal-700 border rounded-lg py-1 px-4">Adopt</button>
+    <button onclick="loadPetDetails('${elements.petId}')" class="btn bg-white text-teal-700 border rounded-lg py-1 px-4">Details</button>
 </div>
 `
 petContainers.appendChild(div);
 });
 
 }
+
+const adoptModal = (event)=>{
+let count = 3 ;
+const countContainer = document.getElementById('countdown-container');
+countContainer.innerText = count;
+my_modal_5.showModal()
+const interval = setInterval(()=>{
+count--
+countContainer.innerText = count;
+if(count<1){
+    clearInterval(interval);
+    my_modal_5.close();
+    event.textContent = 'Adopted';
+    event.disabled(true);
+
+}
+
+},1000)
+}
+
 loadCategories();
 loadAllPets();
--- js/utilities.js
+++ js/utilities.js
@@ -1,3 +1,8 @@
+                let storedPetssData = [];
+
+
+
+
 const loadingSpinner = (show)=>{
     const spinner =  document.getElementById('loader')
     if(show){
@@ -24,3 +29,25 @@ console.log(activeButtons);
 activeButtons.classList.add('bg-emerald-100' ,'rounded-full' ,'bordre-red-500', 'border-2');
 activeButtons.classList.remove('rounded-xl');
 }
+
+const like = (imgUrl)=>{
+console.log(imgUrl);
+const imageContainer = document.getElementById('liked-pets');
+const div = document.createElement('div');
+div.innerHTML = `
+ <img class= "rounded-lg" src="${imgUrl}" alt="">
+
+`
+imageContainer.appendChild(div);
+
+}
+            const sort = ()=>{
+                console.log(storedPetssData);
+                loadingSpinner(true);
+                const sortedData = storedPetssData.sort((a,b)=> b.price - a.price);
+                
+                setTimeout(()=>{
+                    loadingSpinner(false);
+                    displayPets(sortedData);
+                },2000)
+            }
